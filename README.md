049a6cab27d620beb38f730760b3fdca3c7a84ba
Sami Karjalainen <sami.karjalainen@opuscapita.com>
2018-02-20 21:45:39 +0200

    Update example template

--- README.md
+++ README.md
@@ -11,8 +11,14 @@ npm install @opuscapita/react-component-template
 ### Demo
 View the [DEMO](https://opuscapita.github.io/react-component-template)
 
-### Dependencies
-List relevant dependencies
+### Builds
+#### UMD
+The default build with compiled styles in the .js file. Also minified version available in the lib/umd directory.
+#### CommonJS/ES Module
+You need to configure your module loader to use `cjs` or `es` fields of the package.json to use these module types.
+Also you need to configure sass loader, since all the styles are in sass format.
+* With webpack use [resolve.mainFields](https://webpack.js.org/configuration/resolve/#resolve-mainfields) to configure the module type.
+* Add [SASS loader](https://github.com/webpack-contrib/sass-loader) to support importing of SASS styles.
 
 ### API
 | Prop name                | Type             | Default                                  | Description                              |
--- src_docs/index.jsx
+++ src_docs/index.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { AppContainer } from 'react-hot-loader';
-import ComponentView from './components/component-view';
+import ExampleContainer from './containers/example.container';
 
 import './app.component.scss';
-import './favicon.ico';
+import './images/favicon.ico';
 
 const renderApp = (Component) => {
   render(
@@ -18,13 +18,13 @@ const renderApp = (Component) => {
   );
 };
 
-renderApp(ComponentView);
+renderApp(ExampleContainer);
 
 // Webpack Hot Module Replacement API
 /* eslint-disable global-require */
 if (module.hot) {
-  module.hot.accept('./components/component-view', () => {
-    const Comp = require('./components/component-view').default;
+  module.hot.accept('./containers/example.container', () => {
+    const Comp = require('./containers/example.container').default;
     renderApp(Comp);
   });
 }

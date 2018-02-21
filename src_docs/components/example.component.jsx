ef2d5c5909602b149ec1e964ef77125cc3ea2d10
Sami Karjalainen <sami.karjalainen@opuscapita.com>
2018-02-21 12:48:18 +0200

    Update

--- src_docs/components/example.component.jsx
+++ src_docs/components/example.component.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import Example from '../../src/index';
 
-export default function ComponentView() {
-  return (
-    <Example />
-  );
+export default class ComponentView extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Example />
+    );
+  }
 }
--- webpack.docs.config.js
+++ webpack.docs.config.js
@@ -122,6 +122,7 @@ const baseConfig = {
       'node_modules',
     ],
     extensions: ['.js', '.jsx'],
+    mainFields: ['es', 'cjs', 'browser', 'module', 'es:next', 'main'],
   },
 };
 
